f1dbd69901f8f9d6e2038554cfee0bde9e9ac3fd
Marco Graziotti <graziottimarco@gmail.com>
2019-02-17 13:44:15 +0100

    Add project files.

---
+++ Tipi-Generici/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tipi_Generici
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stampante printer = new Stampante();
+
+            //stampa una stringa grazie all'overload (firma diversa) metodo Stampa
+            printer.Stampa("Marco");
+
+            //stampa un int grazie sempre all'overload (firma diversa) del metodo Stampa
+            printer.Stampa(30);
+
+            //stampa un qualsiasi tipo specificato grazie all'uso di un tipo generico
+            printer.Stampa<double>(5.20); //potresti anche omettere la specifica del tipo passato (ossia <char>) perché grazie all'"inferenza di tipo" il compilatore lo riconosce da solo
+
+            //uso di due tipi generici
+            printer.Stampa<string, int>("Anno", 1920);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Tipi-Generici/Stampante.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tipi_Generici
+{
+    class Stampante
+    {
+
+        //metodo stampa che vuole come argomento una stringa
+        public void Stampa(string stringa)
+        {
+            Console.WriteLine("Stampa stringa: " + stringa);
+        }
+
+        //overload del primo metodo Stampa, in argomento ora c'è un int
+        public void Stampa(int intero)
+        {
+            Console.WriteLine("Stampa int: " + intero);
+        }
+
+        //metodo generico, che fa uso di tipi generici e può accettare qualsiasi tipo come argomento
+        public void Stampa<T>(T v)
+        {
+            Console.WriteLine("Stampa grazie a tipo generico: " + v);
+        }
+
+        //passaggio di due tipi generici, notare che il segnaposto T, può anche cambiare nome a proprio piacimento
+        public void Stampa<S, T>(S s, T t)
+        {
+            Console.WriteLine("Stampa con due tipi generici, il primo: " + s + " il secondo: " + t);
+        }
+
+    }
+}
